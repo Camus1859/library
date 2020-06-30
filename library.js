266d64b1ea99ccde6a93d08bc1805301a7c20aaa
Anderson Laventure <Laventurepsu1@gmail.com>
2020-06-30 15:21:58 -0400

    updated css,functions,buttons

--- library.js
+++ library.js
@@ -4,9 +4,11 @@ let author;
 let numberOfPages;
 let readOrNot;
 let string;
-
 let submitButton = document.getElementById('submit-button');
+let addBookButton = document.getElementById('add-book-button');
+
 submitButton.addEventListener('click',addBookToLibrary);
+addBookButton.addEventListener('click',addAbook);
 
 function Book(title, author, numberOfPages, readOrNot) {
   this.title = title,
@@ -30,7 +32,7 @@ Book.prototype.info = function() {
 }
 
 function addBookToLibrary(){
-  title = document.getElementById('book-title').value;
+  title = document.getElementById('book-title').value
   author = document.getElementById('author').value;
   numberOfPages = document.getElementById('pages').value;
   readOrNot = document.getElementById('read-or-not').value;
@@ -38,5 +40,26 @@ function addBookToLibrary(){
   myLibrary.push(book1)
   console.log(myLibrary)
   console.log(book1)
-};
 
+ 
+};
+function visibilityOff(){
+  document.getElementById('book-title').style.visibility='hidden';
+  author = document.getElementById('author').style.visibility='hidden';
+  numberOfPages = document.getElementById('pages').style.visibility='hidden';
+  readOrNot = document.getElementById('read-or-not').style.visibility='hidden';
+  userSubmitButton = document.getElementById('submit-button').style.visibility='hidden';
+}
+visibilityOff()
+
+function addAbook() {
+  visibilityOn()
+}
+
+function visibilityOn() {
+  document.getElementById('book-title').style.visibility='visible';
+  author = document.getElementById('author').style.visibility='visible';
+  numberOfPages = document.getElementById('pages').style.visibility='visible';
+  readOrNot = document.getElementById('read-or-not').style.visibility='visible';
+  userSubmitButton = document.getElementById('submit-button').style.visibility='visible';
+}
