6a143b2b749266b87134668ef785fc416afe1169
Anderson Laventure <Laventurepsu1@gmail.com>
2020-07-02 22:24:12 -0400

    updated all

--- library.js
+++ library.js
@@ -7,11 +7,13 @@ let string;
 let bookTitleContainer
 let userButtonsContainer
 let infoLine;
+let trashIcon
 let submitButton = document.getElementById('submit-button');
 let addBookButton = document.getElementById('add-book-button');
 
 userButtonsContainer = document.getElementById('user-buttons-container').style.display = 'none'
 bookTitleContainer = document.getElementById('book-title-container').style.display = 'none'
+// trashIcon = document.getElementById('icon-trash').style.display = 'none'
 
 submitButton.addEventListener('click', addBookToLibrary);
 addBookButton.addEventListener('click', visibilityOn);
@@ -58,12 +60,19 @@ userButtonsContainer = document.getElementById('user-buttons-container').style.d
 function visibilityOn() {
 userButtonsContainer = document.getElementById('user-buttons-container').style.display = ''
 bookTitleContainer = document.getElementById('book-title-container').style.display = 'none'
+trash = document.getElementById('trash').style.display = 'none'
 document.getElementById("myForm").reset()
+console.log('add new book cliked')
+
 }
 
 function submitBookinfo() {
   visibilityOff()
   bookTitleContainer = document.getElementById('book-title-container').style.display = 'grid'
+  trash = document.getElementById('trash').style.display = ''
+
+  createTrashCan()
+
 }
 
 function arrayOrganizer() {
@@ -79,18 +88,49 @@ function arrayOrganizer() {
  allArrayData = `${lastBook.title} ${lastBook.author} ${lastBook.numberOfPages} ${status}`;
  createInfoLine()
 
+
 }
 
 function createInfoLine() {
   for (let i = 0; i < 4; i++) {
     let h3 = document.createElement('h3');
     let newContent = document.createTextNode(allArrayData.split(" ")[i])
+    console.log(newContent)
     h3.classList.add('eachBook')
     h3.appendChild(newContent);
     bookTitleContainer = document.getElementById('book-title-container')
-    x = bookTitleContainer.appendChild(h3)
-    console.log(x)
+    bookTitleContainer.appendChild(h3)
   }
+}
+
+function createTrashCan(){
+  trash = document.getElementById('trash')
+  let divIconTrash = document.createElement('div')                                   
+  divIconTrash.id = 'icon-trash'
+  bookTitleContainer = document.getElementById('book-title-container')
+  trash.appendChild(divIconTrash)
+
+  let trashLid = document.createElement('div')
+  trashLid.classList.add('trash-lid')
+  trashLid.style.backgroundColor =  'White'
+  divIconTrash.append(trashLid)
+
+  let trashContainer = document.createElement('div')
+  trashContainer.classList.add('trash-container')
+  trashContainer.style.backgroundColor = 'White'
+  divIconTrash.append(trashContainer)
+
+  let trash1 = document.createElement('div')
+  trash1.classList.add('trash-line-1')
+  divIconTrash.append(trash1)
+
+  let trash2 = document.createElement('div')
+  trash2.classList.add('trash-line-2')
+  divIconTrash.append(trash2)
+
+  let trash3 = document.createElement('div')
+  trash3.classList.add('trash-line-3')
+  divIconTrash.append(trash3)
 }
 
 
