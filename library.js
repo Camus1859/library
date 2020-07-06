a62286127f7a49c975bf8d284d3e5be19385a5fd
Anderson Laventure <Laventurepsu1@gmail.com>
2020-07-06 18:40:07 -0400

    updated deletefunction and more

--- library.js
+++ library.js
@@ -2,29 +2,31 @@ let myLibrary = [];
 let title;
 let author;
 let numberOfPages;
-let myCheck;
 let string;
-let bookTitleContainer
+let bookTitleContainer = document.getElementById('book-title-container')
 let userButtonsContainer
-let infoLine;
-let trashIcon
-let fa;
+let h3;
 let submitButton = document.getElementById('submit-button');
 let addBookButton = document.getElementById('add-book-button');
 
-userButtonsContainer = document.getElementById('user-buttons-container').style.display = 'none'
-bookTitleContainer = document.getElementById('book-title-container').style.display = 'none'
+function noDisplay() {
+  userButtonsContainer = document.getElementById('user-buttons-container').style.display = 'none'
+  bookTitleContainer = document.getElementById('book-title-container').style.display = 'none'
+}
+
+noDisplay()
+
 submitButton.addEventListener('click', addBookToLibrary);
 addBookButton.addEventListener('click', visibilityOn);
 submitButton.addEventListener('click', submitBookinfo);
+
+
 
 function Book(title, author, numberOfPages) {
   this.title = title,
   this.author = author,
   this.numberOfPages = numberOfPages
 }
-
-
 
 function addBookToLibrary(){
   title = document.getElementById('book-title').value
@@ -35,8 +37,6 @@ function addBookToLibrary(){
 
   let book1 = new Book(title, author, numberOfPages)
   myLibrary.push(book1)
-  console.log(book1)
-  console.log(myLibrary)
   arrayOrganizer()
 };
 
@@ -49,8 +49,6 @@ function visibilityOn() {
 userButtonsContainer = document.getElementById('user-buttons-container').style.display = ''
 bookTitleContainer = document.getElementById('book-title-container').style.display = 'none'
 document.getElementById("myForm").reset()
-console.log('add new book cliked')
-
 }
 
 function submitBookinfo() {
@@ -60,7 +58,6 @@ function submitBookinfo() {
 
 function arrayOrganizer() {
   let lastBook = myLibrary[myLibrary.length - 1]
-  console.log(lastBook.title)
   let checkBox = document.getElementById("checkBox");
   let status;
   if(checkBox.checked == true) {
@@ -80,7 +77,6 @@ Book.prototype.info = function() {
 
   if(checkBox.checked == true) {
     ending = ' already read.';
-    console.log(ending)
   }
   else if(checkBox.checked == false) {
     ending = ' has not been read yet.'
@@ -91,22 +87,79 @@ Book.prototype.info = function() {
 
 function createInfoLine(obj) {
     for(const property in obj){
-      let h3 = document.createElement('h3');
+      h3 = document.createElement('h3');
       let newContent = document.createTextNode(`${obj[property]}`)
       h3.classList.add('eachBook')
       h3.appendChild(newContent);
       bookTitleContainer = document.getElementById('book-title-container')
-      bookTitleContainer.appendChild(h3)
+      x = bookTitleContainer.appendChild(h3)
+        x.setAttribute('data-number', myLibrary.length -1)
   }
   bookTitleContainer.appendChild(createTrashCan())
-  }
+}
 
 function createTrashCan(){
-  div = document.createElement('div')
+  let div = document.createElement('div')
+  div.setAttribute('data-number', myLibrary.length - 1)
   div.id = 'theDiv'
   let trashDiv = document.createElement('i')
   trashDiv.classList = 'fa fa-trash-o fa-2x'
   div.appendChild(trashDiv)
+  trashDiv.addEventListener('click', deleteLine)
+  trashDiv.setAttribute('data-number', myLibrary.length - 1)
+
   return div
 }
+
+// function deleteLine(e) {
+//   dataA = e.target.getAttribute('data-number')
+//   e.target.remove()
+
+//   let dataFromDiv = Array.from(document.querySelectorAll(`h3[data-number="${dataA}"]`))
+//   dataFromDiv.forEach(item => {
+//     bookTitleContainer = document.getElementById('book-title-container')
+//     bookTitleContainer.removeChild(item)
+// })
+    
+//     bookRemoved = myLibrary.splice(myLibrary[dataA], 1)
+//     let iDel =(document.querySelector(`i[data-number="${dataA}"]`))
+
+//     theDiv = document.getElementById('theDiv')
+//     theDiv.removeChild(iDel)
+//     console.log(iDel)
+
+//     bookTitleContainer.removeChild(theDiv)
+// }
+
+function deleteLine(e) {
+  let dataA = e.target.getAttribute('data-number')
+  console.log(dataA)
+ 
+   let dataFromDiv = Array.from(document.querySelectorAll(`h3[data-number="${dataA}"]`))
+   dataFromDiv.forEach(item => {
+     bookTitleContainer = document.getElementById('book-title-container')
+     bookTitleContainer.removeChild(item)
+ })
+     bookRemoved = myLibrary.splice(myLibrary[dataA], 1)
+     console.log(e.target)
+     e.target.parentElement.remove()
+     e.target.remove()
+ }
+
+
+
+// function deleteLine() {
+//   let theBooks = Array.from(document.querySelectorAll('.eachBook'))
+//     theBooks.forEach(book => {
+//       data = book.getAttribute('data-number') 
+//       console.log(data)
+//     })
+//       for(let i = 0; i < myLibrary.length; i++) {
+//         num = myLibrary.indexOf(myLibrary[i])
+//         console.log(num)
+//         if (num == data) {
+//       }
+//     }
+//   }
+
 
